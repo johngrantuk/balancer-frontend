071a91e6690eade608a7ed38feadef614517a472
johngrantuk <johngrantuk@googlemail.com>
2021-02-01 16:00:15 +0000

    Full deploy.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
                     class="logo"
                     :src="logo"
                 >
-                <span class="title">Balancer Test 5</span>
+                <span class="title">Balancer Test 6</span>
             </div>
         </router-link>
         <div class="header-right">
